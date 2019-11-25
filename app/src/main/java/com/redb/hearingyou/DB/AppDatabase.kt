61c365f93820037f71f09caae19d43d91a48a754
Raúl Duarte Beltrán <47326612+RaulDuarteBeltran@users.noreply.github.com>
2019-11-25 08:35:10 -0600

    Agregado de librerias y esquema del proyecto
    Agruegue las dependencias para usar RoomDatabase y la libreria Stetho.
    Al mismo tiempo agregue el esquema de lo que sera el proyecto con las capas DB, Modelos, VistaModelos y Vistas

---
+++ app/src/main/java/com/redb/hearingyou/DB/AppDatabase.kt
@@ -0,0 +1,54 @@
+package com.redb.hearingyou.DB
+
+import android.content.Context
+import androidx.room.Database
+import androidx.sqlite.db.SupportSQLiteDatabase
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(
+    entities =[
+
+    ],
+    version = 1
+)
+
+abstract class AppDatabase:RoomDatabase(){
+
+    //Instancias de los Daos
+
+    //Objeto singleton
+    companion object {
+
+        private var INSTANCE: AppDatabase? = null
+
+        fun getAppDatabase(context: Context): AppDatabase {
+            if (INSTANCE == null) {
+                INSTANCE = Room.databaseBuilder(
+                    context.getApplicationContext(),
+                    AppDatabase::class.java,
+                    "HearingYou.db"
+                )
+                    .allowMainThreadQueries()
+                    .addCallback(object : RoomDatabase.Callback() {
+                        override fun onCreate(db: SupportSQLiteDatabase) {
+                            super.onCreate(db)
+                            initializeData(db)
+                        }
+                    })
+                    .build()
+            }
+
+            return INSTANCE as AppDatabase
+        }
+
+        fun initializeData(db: SupportSQLiteDatabase) {
+            db.beginTransaction()
+
+
+
+            db.setTransactionSuccessful();
+            db.endTransaction();
+        }
+    }
+}
--- app/src/main/java/com/redb/hearingyou/Vistas/MainActivity.kt
+++ app/src/main/java/com/redb/hearingyou/Vistas/MainActivity.kt
@@ -1,12 +1,18 @@
-package com.redb.hearingyou
+package com.redb.hearingyou.Vistas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.facebook.stetho.Stetho
+import com.redb.hearingyou.DB.AppDatabase
+import com.redb.hearingyou.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Stetho.initializeWithDefaults(this)
+
     }
 }
